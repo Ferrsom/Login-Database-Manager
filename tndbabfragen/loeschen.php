5214ed23a28760ea357357bb0299eda5f63a2c40
Ferrsom <melchor_daniel@hotmail.com>
2026-01-22 20:02:13 +0100

    Remove commented-out SQL query in loeschen.php

--- tndbabfragen/loeschen.php
+++ tndbabfragen/loeschen.php
@@ -3,12 +3,11 @@ include "../db/dbverbindung.php";
 
 if(isset($_GET['tidnummer'])){
     $tID=htmlspecialchars($_GET['tidnummer']);
-    //SQL Abfrage
+
     $sql="DELETE FROM teilnehmer WHERE tID=?";
     $cmd = $verbindung->prepare($sql);
     $cmd->execute([$tID]);
 
-    //
     header("location:../index.php");
     exit;
 
