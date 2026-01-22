05cfb3e0e0d2ac771cd0b8b37cc6a83b5b37b042
Ferrsom <melchor_daniel@hotmail.com>
2026-01-22 20:00:46 +0100

    Refactor login logic for clarity and structure

--- auth/login.php
+++ auth/login.php
@@ -5,21 +5,21 @@ include "../db/dbverbindung.php";
 if(isset($_POST['login'])){
     $benutzername = htmlspecialchars($_POST['benutzername']);
     $passwort = htmlspecialchars($_POST['passwort']);
-    //SQL Abfrage
+
     $sql = "SELECT * FROM benutzer WHERE benutzername=?";
     $cmd = $verbindung->prepare($sql);
     $cmd->execute([$benutzername]);
-    //Uberprüfen ob die Benutzername gefunden wurde
-    if($cmd->rowCount()>0){ //ob es Benutzer gibt
-        //Benutzer Datei
+
+    if($cmd->rowCount()>0){
+
         $benutzer = $cmd->fetch();
-        //Passwort uberprüfen
+
         if(password_verify($passwort, $benutzer['passwort'])){
-            //Session einsetzen
+
             $_SESSION['benutzer_id'] = $benutzer['bid'];
             header("location:../index.php");
             exit;
-        }else{ //Benutzer wahr Passwort falsch
+        }else{
             header("location:../loginseite.php?passwort=f");
             exit;
         }
