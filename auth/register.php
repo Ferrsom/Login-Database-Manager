4842cde3fc7d484870d0368115c501316b0d20d0
Ferrsom <melchor_daniel@hotmail.com>
2026-01-22 20:01:20 +0100

    Clean up register.php by removing comments and whitespace
    Removed commented-out code and unnecessary whitespace.

--- auth/register.php
+++ auth/register.php
@@ -5,17 +5,16 @@ if(isset($_POST['register'])){
     $benutzername = htmlspecialchars($_POST['benutzername']);
     $passwort1 = htmlspecialchars($_POST['passwort1']);
     $passwort2 = htmlspecialchars($_POST['passwort2']);
-    //uberprüfen ob passwort1=passwort2
+
     if($passwort1==$passwort2 && strlen($passwort1)>8){
-        //sichere Passwort 
+
         $sicherpass = password_hash($passwort1, PASSWORD_DEFAULT);
-        //password_verify(122321212, lklkjsdfkjsdflkjasdkl;fjasdfiouoieru3487)
-        //sql Abfrage
+
         $sql = "INSERT into benutzer (benutzername, passwort) VALUES (?, ?) ";
-        //prepare statement
+
         $cmd = $verbindung->prepare($sql);
         $cmd->execute([$benutzername, $sicherpass]);
-        //Umleitung
+
         header("location:../loginseite.php");
         exit;
 
@@ -25,15 +24,7 @@ if(isset($_POST['register'])){
     }
 
 
-
-
-
-
 }
-
-
-
-
 
 
 ?>
